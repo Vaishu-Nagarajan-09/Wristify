48a55f7776f4479b1535f2539bb606f9a8975910
vaishnavi <nnvaishnavi1964@gmail.com>
2026-08-16 12:33:14 +0530

    Day 3 Complete Final Updates

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/Navbar";
+import AppRoutes from "./routes/AppRoutes"
 
 
 const App  = () => {
